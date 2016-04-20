3d2749a59e74a6357e7cced11556894166ac8c16
Emanuel Moser <emanuel.moser@student.tugraz.at>
2016-04-20 08:38:41 +0200

    [CLEM] prepared directory for testing

---
+++ app/src/androidTest/java/nudelsquad/nudelcalendar/test/firstTest.java
@@ -0,0 +1,7 @@
+package nudelsquad.nudelcalendar.test;
+
+/**
+ * Created by emanuel on 20/04/16.
+ */
+public class firstTest {
+}
---
+++ app/src/androidTest/java/nudelsquad/nudelcalendar/uitest/firstUITest.java
@@ -0,0 +1,7 @@
+package nudelsquad.nudelcalendar.uitest;
+
+/**
+ * Created by emanuel on 20/04/16.
+ */
+public class firstUITest {
+}
