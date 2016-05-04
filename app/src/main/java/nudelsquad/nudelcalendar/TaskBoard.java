4200b22bb70b69b97b59d46692ebda9102075440
Clemens Stary <c.stary@student.tugraz.at>
2016-05-04 14:28:09 +0200

    [BW;CW]click listener added and item row changed

--- app/src/main/java/nudelsquad/nudelcalendar/TaskBoard.java
+++ app/src/main/java/nudelsquad/nudelcalendar/TaskBoard.java
@@ -1,57 +1,94 @@
 package nudelsquad.nudelcalendar;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 /**
  * Created by waser2 on 27.04.2016.
  */
-public class TaskBoard extends Fragment{
-ListView taskboardview;
+public class TaskBoard extends Fragment {
+    ListView taskboardview;
 
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 
-        View rootView = inflater.inflate(R.layout.activity_taskboard,container,false);
+        View rootView = inflater.inflate(R.layout.activity_taskboard, container, false);
         Bundle args = getArguments();
-        taskboardview = (ListView)rootView.findViewById(R.id.taskboard_list);
+        taskboardview = (ListView) rootView.findViewById(R.id.taskboard_list);
         List<Task> tasklist = new ArrayList<Task>();
         tasklist.add(new Task("Task 12", 200, "ho"));
-        tasklist.add(new Task("Task Müll wegraumen",50, "Notes bitte nicht vergessen..."));
-        tasklist.add(new Task("Task Müll wegraumen",60, "Notes bitte nicht vergessen..."));
-        tasklist.add(new Task("Task Müll wegraumen",90, "NoteBLABLABLBABALBALBALBen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 50, "Notes bitte nicht vergessen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 60, "Notes bitte nicht vergessen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 90, "NoteBLABLABLBABALBALBALBen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 50, "Notes bitte nicht vergessen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 60, "Notes bitte nicht vergessen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 90, "NoteBLABLABLBABALBALBALBen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 50, "Notes bitte nicht vergessen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 60, "Notes bitte nicht vergessen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 90, "NoteBLABLABLBABALBALBALBen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 50, "Notes bitte nicht vergessen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 60, "Notes bitte nicht vergessen..."));
+        tasklist.add(new Task("Task Müll wegraumen", 90, "NoteBLABLABLBABALBALBALBen..."));
 
-      //  String[] test = new String[]{"23","44","55"};
+        //  String[] test = new String[]{"23","44","55"};
 
 
         TaskBoardAdapter adapter = new TaskBoardAdapter(rootView.getContext(), tasklist);
 
-        ListView listView = (ListView) rootView.findViewById(R.id.taskboard_list);
+        final ListView listView = (ListView) rootView.findViewById(R.id.taskboard_list);
         listView.setAdapter(adapter);
 
-        TextView txTaskHeader = (TextView)rootView.findViewById(R.id.txTasks);
 
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Object item = listView.getAdapter().getItem(position);
+                TextView description = (TextView) view.findViewById(R.id.txt_descpriton);
 
+                int visibility = description.getVisibility();
+                if (visibility == View.GONE)
+                    description.setVisibility(View.VISIBLE);
+                else
+                    description.setVisibility(View.GONE);
+
+                return true;
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getContext(), "Link click", Toast.LENGTH_LONG).show();
+            }
+        });
 
         return rootView;
 
 
-
-
     }
 }
+
+
+
+
