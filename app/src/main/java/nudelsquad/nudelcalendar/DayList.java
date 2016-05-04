d07582e11bc718fb36138d66c2539252ccacfbc5
Benjamin Waser <benjamin.waser@student.tugraz.at>
2016-05-04 14:27:38 +0200

    [BWCS] - CreateTask Layout implemented
    create Task layout should be finish now

--- app/src/main/java/nudelsquad/nudelcalendar/DayList.java
+++ app/src/main/java/nudelsquad/nudelcalendar/DayList.java
@@ -24,8 +24,7 @@ public class DayList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //setContentView(R.layout.activity_day_list);
-        View rootView = inflater.inflate(
-                R.layout.activity_day_list, container, false);
+        View rootView = inflater.inflate(R.layout.activity_day_list, container, false);
         Bundle args = getArguments();
         daylist = (ListView)rootView.findViewById(R.id.day_eventlist);
         List<EventBean> eventlist = new ArrayList<EventBean>();
--- app/src/main/java/nudelsquad/nudelcalendar/TaskBoard.java
+++ app/src/main/java/nudelsquad/nudelcalendar/TaskBoard.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,6 +15,7 @@ import android.support.v4.app.Fragment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by waser2 on 27.04.2016.
@@ -30,7 +32,7 @@ ListView taskboardview;
         View rootView = inflater.inflate(R.layout.activity_taskboard,container,false);
         Bundle args = getArguments();
         taskboardview = (ListView)rootView.findViewById(R.id.taskboard_list);
-        List<Task> tasklist = new ArrayList<Task>();
+        final List<Task> tasklist = new ArrayList<Task>();
         tasklist.add(new Task("Task 12", 200, "ho"));
         tasklist.add(new Task("Task Müll wegraumen",50, "Notes bitte nicht vergessen..."));
         tasklist.add(new Task("Task Müll wegraumen",60, "Notes bitte nicht vergessen..."));
@@ -41,11 +43,33 @@ ListView taskboardview;
 
         TaskBoardAdapter adapter = new TaskBoardAdapter(rootView.getContext(), tasklist);
 
-        ListView listView = (ListView) rootView.findViewById(R.id.taskboard_list);
+        final ListView listView = (ListView) rootView.findViewById(R.id.taskboard_list);
         listView.setAdapter(adapter);
+        listView.setClickable(true);
 
         TextView txTaskHeader = (TextView)rootView.findViewById(R.id.txTasks);
 
+        /*
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+           // public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //view = this.inflater.inflate(R.layout.task_item,parent,false);
+           //     String message = "abc";
+            //    intent.putExtra(EXTRA_MESSAGE, message);
+             //   startActivity(intent)
+
+       //    }
+
+
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               // Objects listItem = taskboardview.getItemAtPosition(position);
+
+                TextView test = (TextView) rootView.findViewById(R.id.tv_expand);
+            test.setText("DASSS IST DIE NOTE");
+            }
+        });
+*/
 
 
         return rootView;
