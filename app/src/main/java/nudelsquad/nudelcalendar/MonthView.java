f23b8af1ec2e3f11a91c6ce493bde00f12da0c16
CrazyChristoph <christoph.ladler@gmail.com>
2016-05-04 09:15:59 +0200

    [CLSK] MonthView implemented and Button linked

--- app/src/main/java/nudelsquad/nudelcalendar/MonthView.java
+++ app/src/main/java/nudelsquad/nudelcalendar/MonthView.java
@@ -1,8 +1,44 @@
 package nudelsquad.nudelcalendar;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.CalendarView.OnDateChangeListener;
+import android.widget.Toast;
+
 /**
  * Created by Sathearo on 27.04.2016.
  */
-public class MonthView {
+public class MonthView extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.month_fragment, container, false);
+
+        CalendarView calendarView = (CalendarView) rootView.findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                Fragment fragment = new DayList();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                transaction.replace(R.id.main_frame, fragment);
+                transaction.commit();
+            }
+        });
+
+        return rootView;
+
+    };
 
 }
