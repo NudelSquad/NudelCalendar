2af95bf88b6bdc9d6e31a1a862a915ead8d032e5
Benjamin Waser <benjamin.waser@student.tugraz.at>
2016-05-04 16:21:20 +0200

    [BWCS] - activity of create Task
    activity was created  (not only the layout)

--- app/src/main/java/nudelsquad/nudelcalendar/TaskBoard.java
+++ app/src/main/java/nudelsquad/nudelcalendar/TaskBoard.java
@@ -19,11 +19,10 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
-<<<<<<< HEAD
+
 import java.util.Objects;
-=======
 import java.util.zip.Inflater;
->>>>>>> createTask
+
 
 /**
  * Created by waser2 on 27.04.2016.
@@ -39,13 +38,10 @@ public class TaskBoard extends Fragment {
 
         View rootView = inflater.inflate(R.layout.activity_taskboard, container, false);
         Bundle args = getArguments();
-<<<<<<< HEAD
-        taskboardview = (ListView)rootView.findViewById(R.id.taskboard_list);
-        final List<Task> tasklist = new ArrayList<Task>();
-=======
+
         taskboardview = (ListView) rootView.findViewById(R.id.taskboard_list);
         List<Task> tasklist = new ArrayList<Task>();
->>>>>>> createTask
+
         tasklist.add(new Task("Task 12", 200, "ho"));
         tasklist.add(new Task("Task Müll wegraumen", 50, "Notes bitte nicht vergessen..."));
         tasklist.add(new Task("Task Müll wegraumen", 60, "Notes bitte nicht vergessen..."));
@@ -59,9 +55,7 @@ public class TaskBoard extends Fragment {
         tasklist.add(new Task("Task Müll wegraumen", 50, "Notes bitte nicht vergessen..."));
         tasklist.add(new Task("Task Müll wegraumen", 60, "Notes bitte nicht vergessen..."));
         tasklist.add(new Task("Task Müll wegraumen", 90, "NoteBLABLABLBABALBALBALBen..."));
-
-        //  String[] test = new String[]{"23","44","55"};
-
+        
 
         TaskBoardAdapter adapter = new TaskBoardAdapter(rootView.getContext(), tasklist);
 
@@ -70,35 +64,12 @@ public class TaskBoard extends Fragment {
         listView.setClickable(true);
 
 
-<<<<<<< HEAD
-        /*
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-           // public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-
-                //view = this.inflater.inflate(R.layout.task_item,parent,false);
-           //     String message = "abc";
-            //    intent.putExtra(EXTRA_MESSAGE, message);
-             //   startActivity(intent)
-
-       //    }
-
-
-           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               // Objects listItem = taskboardview.getItemAtPosition(position);
-
-                TextView test = (TextView) rootView.findViewById(R.id.tv_expand);
-            test.setText("DASSS IST DIE NOTE");
-            }
-        });
-*/
-=======
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 Object item = listView.getAdapter().getItem(position);
                 TextView description = (TextView) view.findViewById(R.id.txt_descpriton);
->>>>>>> createTask
+
 
                 int visibility = description.getVisibility();
                 if (visibility == View.GONE)
