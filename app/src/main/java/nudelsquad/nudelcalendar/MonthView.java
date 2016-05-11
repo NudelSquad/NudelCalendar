1a8b86dd9783d11869a7f4204295cfe31b65c91e
Sathearo Kim <sath.k1@gmx.at>
2016-05-11 14:37:50 +0200

    [CLSK] - MonthView linked

--- app/src/main/java/nudelsquad/nudelcalendar/MonthView.java
+++ app/src/main/java/nudelsquad/nudelcalendar/MonthView.java
@@ -1,5 +1,9 @@
 package nudelsquad.nudelcalendar;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,8 +18,12 @@ import android.widget.CalendarView.OnDateChangeListener;
 import android.widget.Toast;
 
 import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 /**
  * Created by Sathearo on 27.04.2016.
@@ -38,8 +46,60 @@ public class MonthView extends Fragment {
         ft.replace(R.id.calendarFragment, caldroidFragment, "NewFragmentTag");
         ft.commit();
 
-        return rootView;
+        Calendar c1 = GregorianCalendar.getInstance();
+        c1.set(2016, Calendar.MAY, 15);
+        Date greenDate = c1.getTime();
+        ColorDrawable green = new ColorDrawable(Color.GREEN);
 
-    };
+        caldroidFragment.setBackgroundDrawableForDate(green, greenDate);
+
+        c1.set(2016, Calendar.MAY, 16);
+        Date blueDate = c1.getTime();
+        ColorDrawable blue = new ColorDrawable(Color.BLUE);
+
+        caldroidFragment.setBackgroundDrawableForDate(blue, blueDate);
+
+
+        caldroidFragment.refreshView();
+
+        final CaldroidListener listener = new CaldroidListener() {
+
+            @Override
+            public void onSelectDate(Date date, View view) {
+                final FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.main_frame, new DayList(), "NewFragmentTag");
+                ft.commit();
+            }
+
+            @Override
+            public void onChangeMonth(int month, int year) {
+                //String text = "month: " + month + " year: " + year;
+                //Toast.makeText(rootView.getContext(), text,
+                //Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClickDate(Date date, View view) {
+                //Toast.makeText(rootView.getContext(),
+                //      "Long click " + "Date = " + date,
+                //      Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCaldroidViewCreated() {
+                //Toast.makeText(rootView.getContext(),
+                //"Caldroid view is created",
+                //Toast.LENGTH_SHORT).show();
+            }
+
+        };
+
+        caldroidFragment.setCaldroidListener(listener);
+
+        return rootView;
+    }
+
+
+    ;
 
 }
