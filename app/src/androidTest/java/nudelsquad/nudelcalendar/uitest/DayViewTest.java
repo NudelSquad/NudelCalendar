01c098e780be94d5647b21514d93a92c32da01d1
Clemens Stary <c.stary@student.tugraz.at>
2016-05-18 09:43:00 +0200

    [BW,CS] Testcases

--- app/src/androidTest/java/nudelsquad/nudelcalendar/uitest/DayViewTest.java
+++ app/src/androidTest/java/nudelsquad/nudelcalendar/uitest/DayViewTest.java
@@ -12,11 +12,11 @@ import nudelsquad.nudelcalendar.R;
 /**
  * Created by emanuel on 20/04/16.
  */
-public class SettingsViewTest extends ActivityInstrumentationTestCase2<MainActivity> {
+public class DayViewTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private Solo solo;
 
-    public SettingsViewTest() {
+    public DayViewTest() {
         super(MainActivity.class);
     }
 
@@ -24,47 +24,11 @@ public class SettingsViewTest extends ActivityInstrumentationTestCase2<MainActiv
         super.setUp();
         solo = new Solo(getInstrumentation(), getActivity());
         openNavigationDrawer();
-        solo.clickOnText("Settings");
+        solo.clickOnText("Day");
         solo.sleep(500);
 
     }
 
-
-    public void testSwitcher() {
-        View swLS = getActivity().findViewById(R.id.swLS);
-        View swRem = getActivity().findViewById(R.id.swRem);
-        solo.clickOnView(swLS);
-        solo.clickOnView(swRem);
-    }
-
-    public void testComboBox() {
-        View view = getActivity().findViewById(R.id.spMuteFor);
-        solo.clickOnView(view);
-        boolean b;
-
-        b = solo.searchText("15");
-        assertTrue(b);
-        b = solo.searchText("30");
-        assertTrue(b);
-        b = solo.searchText("45");
-        assertTrue(b);
-        b = solo.searchText("60");
-        assertTrue(b);
-        b = solo.searchText("75");
-        assertTrue(b);
-
-        solo.clickInList(2);
-
-
-
-    }
-
-    public void testButton() {
-        solo.clickOnButton("Save");
-        solo.sleep(500);
-        boolean saved = solo.searchText("Saved");
-        assertTrue(saved);
-    }
 
 
     public void openNavigationDrawer() {
