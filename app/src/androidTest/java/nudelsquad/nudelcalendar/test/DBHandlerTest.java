caef7db93c8517f249143bc07a23d6c37ae8649a
Marco <m.stranner@student.tugraz.at>
2016-05-18 13:16:37 +0200

    [MSAR] new test cases DBHandler

--- app/src/androidTest/java/nudelsquad/nudelcalendar/test/DBHandlerTest.java
+++ app/src/androidTest/java/nudelsquad/nudelcalendar/test/DBHandlerTest.java
@@ -12,8 +12,9 @@ import nudelsquad.nudelcalendar.Task;
  * Created by Marco on 18.05.2016.
  */
 public class DBHandlerTest extends AndroidTestCase {
-    Event testEvent = new Event("TestEvent", "17:00", "18:00", "18-05-2016", "TestType", "TestLocation", -1);
-    Task testTask = new Task("TestTask", "18.05.2016", "blabla", -1, -1, "false");
+    Event testEvent = new Event("TestEvent", "17:00", "18:00", "18-05-2016", "TestType", "TestLocation", 123);
+    Event testEventUpdate = new Event(1, "TestEvent1", "17:01", "18:01", "19-05-2016", "TestType1", "TestLocation1", 124);
+    Task testTask = new Task("TestTask", "18.05.2016", "blabla", 123, 1, "false");
     DBHandler dbh;
 
     @Override
@@ -21,16 +22,41 @@ public class DBHandlerTest extends AndroidTestCase {
         super.setUp();
         RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
         dbh = new DBHandler(context);
+        dbh.resetDatabase();
+        dbh.addEvent(testEvent);
+        dbh.addTask(testTask);
     }
 
-    public void testAddEvent(){
-        dbh.addEvent(testEvent);
-    }
+    //------------------------------Test Table Event
 
     public void testGetEvents(){
         List<Event> events = dbh.getAllEvents();
         for (Event e:events) {
-            if (e.getEVENT_COLOR() == -1){
+            if (e.getEVENT_ID() == 1){
+                assertEquals(e.getEVENT_NAME(), testEvent.getEVENT_NAME());
+                assertEquals(e.getEVENT_START(), testEvent.getEVENT_START());
+                assertEquals(e.getEVENT_END(), testEvent.getEVENT_END());
+                assertEquals(e.getEVENT_DATUM(), testEvent.getEVENT_DATUM());
+                assertEquals(e.getEVENT_TYPE(), testEvent.getEVENT_TYPE());
+                assertEquals(e.getEVENT_LOCATION(), testEvent.getEVENT_LOCATION());
+            }
+        }
+    }
+
+    public void testGetEvent(){
+        Event e = dbh.getEvent(1);
+        assertEquals(e.getEVENT_NAME(), testEvent.getEVENT_NAME());
+        assertEquals(e.getEVENT_START(), testEvent.getEVENT_START());
+        assertEquals(e.getEVENT_END(), testEvent.getEVENT_END());
+        assertEquals(e.getEVENT_DATUM(), testEvent.getEVENT_DATUM());
+        assertEquals(e.getEVENT_TYPE(), testEvent.getEVENT_TYPE());
+        assertEquals(e.getEVENT_LOCATION(), testEvent.getEVENT_LOCATION());
+    }
+
+    public void testGetEventsOfDay(){
+        List<Event> events = dbh.getEventsFromDay(testEvent.getEVENT_DATUM());
+        for (Event e:events) {
+            if (e.getEVENT_ID() == 1){
                 assertEquals(e.getEVENT_NAME(), testEvent.getEVENT_NAME());
                 assertEquals(e.getEVENT_START(), testEvent.getEVENT_START());
                 assertEquals(e.getEVENT_END(), testEvent.getEVENT_END());
@@ -44,7 +70,7 @@ public class DBHandlerTest extends AndroidTestCase {
     public void testGetEventsFromMonthOfYear(){
         List<Event> events = dbh.getEventsFromMonthOfYear("05-2016");
         for (Event e:events) {
-            if (e.getEVENT_COLOR() == -1){
+            if (e.getEVENT_ID() == 1){
                 assertEquals(e.getEVENT_NAME(), testEvent.getEVENT_NAME());
                 assertEquals(e.getEVENT_START(), testEvent.getEVENT_START());
                 assertEquals(e.getEVENT_END(), testEvent.getEVENT_END());
@@ -55,8 +81,34 @@ public class DBHandlerTest extends AndroidTestCase {
         }
     }
 
-    public void testAddTask(){
-        dbh.addTask(testTask);
+    public void testUpdateEvent(){
+        dbh.updateEvent(testEventUpdate);               //Update Event with ID 1
+        Event e = dbh.getEvent(1);
+        assertEquals(e.getEVENT_NAME(), testEventUpdate.getEVENT_NAME());
+        assertEquals(e.getEVENT_START(), testEventUpdate.getEVENT_START());
+        assertEquals(e.getEVENT_END(), testEventUpdate.getEVENT_END());
+        assertEquals(e.getEVENT_DATUM(), testEventUpdate.getEVENT_DATUM());
+        assertEquals(e.getEVENT_TYPE(), testEventUpdate.getEVENT_TYPE());
+        assertEquals(e.getEVENT_LOCATION(), testEventUpdate.getEVENT_LOCATION());
+    }
+
+    public void testGetEventsCount(){
+        int count = dbh.getEventsCount();
+        assertEquals(count, 1);
+    }
+
+    public void testDeleteEvent(){
+        dbh.deleteEvent(testEventUpdate);
+        assertEquals(dbh.getEventsCount(), 0);
+    }
+
+    public void testGetTask(){
+        Task t = dbh.getTask(1);
+        assertEquals(t.getTASK_NAME(), testTask.getTASK_NAME());
+        assertEquals(t.getTASK_DATUM(), testTask.getTASK_DATUM());
+        assertEquals(t.getTASK_TEXT(), testTask.getTASK_TEXT());
+        assertEquals(t.getTASK_EVENTID(), testTask.getTASK_EVENTID());
+        assertEquals(t.getTASK_CHECKED(), testTask.getTASK_CHECKED());
     }
 
     public void testGetTasks(){
