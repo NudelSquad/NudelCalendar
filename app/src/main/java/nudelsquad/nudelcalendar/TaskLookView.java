c6f5753e8cab6867440d1b5dd5412adaa57aff8f
Christoph Ladler <christoph.ladler@gmail.com>
2016-06-01 08:51:48 +0200

    [CL,CS] Delete Task in TaskLookView

--- app/src/main/java/nudelsquad/nudelcalendar/TaskLookView.java
+++ app/src/main/java/nudelsquad/nudelcalendar/TaskLookView.java
@@ -1,15 +1,20 @@
 package nudelsquad.nudelcalendar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Marco on 04.05.2016.
@@ -17,6 +22,7 @@ import android.widget.TextView;
 public class TaskLookView extends Fragment {
     private View rootView;
     private int taskID = -1;
+    private DBHandler dbh;
 
     public TaskLookView(int taskID) {
         this.taskID = taskID;
@@ -31,8 +37,10 @@ public class TaskLookView extends Fragment {
         return rootView;
     }
 
+
+
     public void initGUI(){
-        DBHandler dbh = new DBHandler(rootView.getContext());
+        dbh = new DBHandler(rootView.getContext());
         Task t = dbh.getTask(taskID);
         FrameLayout color = (FrameLayout) rootView.findViewById(R.id.task_color);
         color.setBackgroundColor(t.getTASK_COLOR());
@@ -44,5 +52,43 @@ public class TaskLookView extends Fragment {
         tx.setText(t.getTASK_TEXT());
         CheckBox cb = (CheckBox) rootView.findViewById(R.id.cb_task_reminder);
         cb.setChecked(true);
+
+        Button delete = (Button) rootView.findViewById(R.id.btn_delete_task);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert1 = new AlertDialog.Builder(rootView.getContext());
+                alert1.setMessage("Delete Task??!")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dbh.deleteTask(taskID);
+                                final FragmentTransaction ft = getFragmentManager().beginTransaction();
+                                ft.replace(R.id.main_frame, new TaskBoard(), "NewFragmentTag");
+                                ft.commit();
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert3 = alert1.create();
+                alert3.setTitle("DELETE TASK");
+                alert3.show();
+            }
+        });
+
+        Button edit = (Button) rootView.findViewById(R.id.btn_edit_task);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(rootView.getContext(), "Clicked on Edit", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
