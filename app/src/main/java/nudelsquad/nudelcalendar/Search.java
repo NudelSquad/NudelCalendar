c2e798e02a010adb0307acc8a8625918c8410f31
Benjamin Waser <benjamin.waser@student.tugraz.at>
2016-06-01 12:35:34 +0200

    [BWSK] Search field on each layout file
    Search layout field is implemented at the required layouts (without function)

---
+++ app/src/main/java/nudelsquad/nudelcalendar/Search.java
@@ -0,0 +1,16 @@
+package nudelsquad.nudelcalendar;
+
+import android.support.v4.app.Fragment;
+
+import java.util.List;
+
+/**
+ * Created by waser2 on 01.06.2016.
+ */
+public class Search extends Fragment {
+    List<Task> tasklist;
+    List<Event>eventList;
+
+
+
+}
