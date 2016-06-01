fde52d47e5f9954763a17f7ea5eda5fff7ac3564
Benjamin Waser <benjamin.waser@student.tugraz.at>
2016-06-01 16:16:01 +0200

    [BWSK] - ListView for Search update

--- app/src/main/java/nudelsquad/nudelcalendar/search_entrys.java
+++ app/src/main/java/nudelsquad/nudelcalendar/search_entrys.java
@@ -1,7 +1,11 @@
 package nudelsquad.nudelcalendar;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class search_entrys extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class search_entrys extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_entrys);
+
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            mySearch(query);
+            Toast.makeText(getApplicationContext(), query, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void mySearch(String querr)
+    {
+        int a=5;
+        int b=3;
     }
 }
